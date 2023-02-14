dd57a477039aab34b915f921bda2a7f7241093cc
Elie Gaboriau <elie@opensourcepolitics.eu>
2023-02-14 15:56:31 +0100

    initial commits

--- db/migrate/20230214141859_create_decidim_statuses.rb
+++
@@ -1,9 +0,0 @@
-class CreateDecidimStatuses < ActiveRecord::Migration[6.1]
-  def change
-    create_table :decidim_statuses do |t|
-      t.jsonb :name
-      t.references :decidim_organization, foreign_key: true, index: true
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20230214141937_add_status_to_initiatives.rb
+++
@@ -1,5 +0,0 @@
-class AddStatusToInitiatives < ActiveRecord::Migration[6.1]
-  def change
-    add_reference :decidim_initiatives, :decidim_status, index: true
-  end
-end
